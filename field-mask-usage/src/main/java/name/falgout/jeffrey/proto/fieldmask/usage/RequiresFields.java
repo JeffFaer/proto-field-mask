c421f1f0bc3a266789166a0f97fb81488299745b
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2018-05-19 22:04:55 -0600

    Restrict @RequriesFields to parameters.
    That's the only use we're currently validating in FieldUsageValidator.

--- field-mask-usage/src/main/java/name/falgout/jeffrey/proto/fieldmask/usage/RequiresFields.java
+++ field-mask-usage/src/main/java/name/falgout/jeffrey/proto/fieldmask/usage/RequiresFields.java
@@ -1,12 +1,15 @@
 package name.falgout.jeffrey.proto.fieldmask.usage;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Can be used to annotate a proto type with information about which fields will be accessed.
  */
 @Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.PARAMETER)
 public @interface RequiresFields {
   String[] value();
 }
